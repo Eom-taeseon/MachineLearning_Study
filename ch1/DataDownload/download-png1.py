0e688fc9c1a00775cd1323dc1183861360deb8e2
Eom-taeseon <umts2358@naver.com>
2020-02-28 13:04:08 +0900

    Chapter 1_test

---
+++ ch1/DataDownload/download-png1.py
@@ -0,0 +1,10 @@
+# 라이브러리 읽어 들이기
+import urllib.request
+
+
+url = "http://uta.pw/shodou/img/28/214.png"
+savename = "test.png"
+
+#다운로드
+urllib.request.urlretrieve(url, savename)
+print("저장되었습니다...!")
